f9187af801044c81067eea4d29ee5031f066df47
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-08-28 21:41:43 +0530

    add button style, further css style for display and container.

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function digitMaker() {
     for (i=9; i>=0; i--) {
     let digitButton = document.createElement("button");
     digitButton.innerText = i;
+    digitButton.classList.add('button')
     digitButton.classList.add(i)
     document.getElementById('button-box').appendChild(digitButton);
 }}
 
 digitMaker();
-
