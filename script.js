0941b3c752db44963d79f8ee374496df86a0f8b4
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-08-29 19:53:12 +0530

    change style for button, fix button layout with necessary size changes.

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function digitMaker() {
-    
-    for (i=9; i>=0; i--) {
-    let digitButton = document.createElement("button");
-    digitButton.innerText = i;
-    digitButton.classList.add('button')
-    digitButton.classList.add(i)
-    document.getElementById('button-box').appendChild(digitButton);
-}}
 
-digitMaker();
