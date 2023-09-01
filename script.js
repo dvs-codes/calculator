08ae6da13e3bbc4c84cc10b0659252a0d6e9f9ec
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-09-01 22:39:58 +0530

    add logic for listening for button press.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let digits = document.querySelectorAll('.digits')
+
+// for (let i=0; i<digits.length; i++) {
+//     digits[i] = document.querySelector(`.btn${i}`)
+//     digits[i].addEventListener('click',()=> { console.log(digits[i].innerText)})
+// }
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => { console.log(digit.innerText)})
+})
 
