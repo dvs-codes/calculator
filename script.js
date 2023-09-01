7c77e35accae6580b764a4bab21671db1f9a8afb
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-09-01 22:44:41 +0530

    add logic for to display pressed button on the display.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let digits = document.querySelectorAll('.digits')
+const display = document.querySelector('.display')
 
+console.log(display)
 // for (let i=0; i<digits.length; i++) {
 //     digits[i] = document.querySelector(`.btn${i}`)
 //     digits[i].addEventListener('click',()=> { console.log(digits[i].innerText)})
 // }
 
 digits.forEach((digit) => {
-    digit.addEventListener('click', () => { console.log(digit.innerText)})
+    digit.addEventListener('click', () => {display.innerText = digit.innerText})
 })
 
