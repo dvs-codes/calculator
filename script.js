6918f2982e498bb27a8513ed754780c7579050f7
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-09-02 22:52:28 +0530

    add logic for simple addition operation.

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 let digits = document.querySelectorAll('.digits')
 const display = document.querySelector('.display')
+const additionButton = document.querySelector('.addition')
+const subtractButton = document.querySelector('.subtraction')
+const multiplyButton = document.querySelector('.multiplier')
+const divisionButton = document.querySelector('.divider')
+const equalButton = document.querySelector('.eql')
+display.innerText = "";
 
-console.log(display)
-// for (let i=0; i<digits.length; i++) {
-//     digits[i] = document.querySelector(`.btn${i}`)
-//     digits[i].addEventListener('click',()=> { console.log(digits[i].innerText)})
-// }
+function addition(a,b) {
+    console.log(a+b);
+}
+
+function operate(a,b,operataion) {
+    operataion(a,b)
+}
+
+let result = operate(6,9, addition)
+
 
 digits.forEach((digit) => {
-    digit.addEventListener('click', () => {display.innerText = digit.innerText})
+    digit.addEventListener('click', () => {display.innerText += parseInt(digit.innerText)})
 })
+
+additionButton.addEventListener('click', () => {display.innerText += additionButton.innerText})
+
+equalButton.addEventListener('click', () => {
+    console.log(display.innerText)
+})
+
+console.log(typeof(digits.innerText))
+
+
+
 
