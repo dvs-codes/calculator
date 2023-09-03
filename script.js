8639d82e21250f74f60db2c02eb1dce7a3d9fc1b
DVS-CODES <132570673+DVS-CODES@users.noreply.github.com>
2023-09-03 16:21:12 +0530

    implementation of addition function on the calculator.

--- script.js
+++ script.js
@@ -5,30 +5,67 @@ const subtractButton = document.querySelector('.subtraction')
 const multiplyButton = document.querySelector('.multiplier')
 const divisionButton = document.querySelector('.divider')
 const equalButton = document.querySelector('.eql')
+const clearButton = document.querySelector('.clear')
 display.innerText = "";
 
-function addition(a,b) {
-    console.log(a+b);
-}
-
-function operate(a,b,operataion) {
-    operataion(a,b)
-}
-
-let result = operate(6,9, addition)
-
+let firstNumber = "";
+let secondNumber = "";
+let operator ="";
 
 digits.forEach((digit) => {
-    digit.addEventListener('click', () => {display.innerText += parseInt(digit.innerText)})
+    digit.addEventListener('click', () => {
+    display.innerText += digit.innerText
+    //if operator is not present than digit should be firstNumber, else it should be secondNumber
+    if (!operator) {
+        firstNumber += digit.innerText
+    } else {
+        secondNumber += digit.innerText
+    }
+    console.log(firstNumber)
+    console.log(secondNumber)
+
+})
 })
 
-additionButton.addEventListener('click', () => {display.innerText += additionButton.innerText})
+
+
+clearButton.addEventListener('click', () => {
+    display.innerText = "";
+    firstNumber = "";
+    secondNumber = "";
+    operator ="";
+})
+
+additionButton.addEventListener('click', () => {
+    display.innerText += additionButton.innerText;
+    operator = "+"
+})
+
+subtractButton.addEventListener('click', () => {
+    display.innerText += subtractButton.innerText;
+    operator = "-"
+})
+
+multiplyButton.addEventListener('click', () => {
+    display.innerText += multiplyButton.innerText;
+    operator = "x"
+})
+
+divisionButton.addEventListener('click', () => {
+    display.innerText += divisionButton.innerText;
+    operator = "÷"
+})
 
 equalButton.addEventListener('click', () => {
-    console.log(display.innerText)
+    if (operator === "+") {
+        display.innerText = parseInt(firstNumber) + parseInt(secondNumber);
+        firstNumber = display.innerText;
+        secondNumber = "";
+        operator ="";
+    }
 })
 
-console.log(typeof(digits.innerText))
+
 
 
 
